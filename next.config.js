91de4a0f5361fb91873dd4adeb34918edd680e84
chauhanvipul25 <vipul.chauhan@comprotechnologies.com>
2023-11-30 18:05:30 +0530

    init

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '1337',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'strapi-demo-sibc.onrender.com',
+        pathname: '/uploads/**',
+      }
+    ],
+  },
+}
+
+module.exports = nextConfig
